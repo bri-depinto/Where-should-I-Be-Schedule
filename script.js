e9dfd6a6aa8e1bc96a1ad18c34888f63e003ecda
bri.depinto <briannadepinto@gmail.com>
2024-10-28 14:57:05 -0400

    started some javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    const btn = $("#submitDay");
+
+    const bellSchedule = {
+        1: { start: '8:24 AM', end: '9:31 AM' },
+        2: { start: '9:36 AM', end: '10:43 AM' },
+        3: { start: '10:48 AM', end: '11:55 AM' },
+        4: { start: '12:41 PM', end: '1:48 PM' },
+        5: { start: '1:53 PM', end: '3:00 PM' }
+      };
+
+ const scheduleUrl = "https://api.npoint.io/5a932cb7bf66e8f600e9"
+
+    })
